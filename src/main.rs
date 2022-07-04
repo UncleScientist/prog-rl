9d023a0e344df8ee662a942ee7621606e551dd36
Uncle Scientist <unclescientist@gmail.com>
2022-07-04 10:05:41 -0400

    basic font working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bracket_lib::prelude::*;
 
+embedded_resource!(WIDE_FONT, "resources/terminal_10x16.png");
+embedded_resource!(VGA_FONT, "resources/vga8x16.png");
+
 struct State {}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
@@ -9,8 +12,14 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    link_resource!(WIDE_FONT, "resources/terminal_10x16.png");
+    link_resource!(VGA_FONT, "resources/vga8x16.png");
+
+    let context = BTermBuilder::new()
+        .with_simple_console(80, 50, "terminal_10x16.png")
         .with_title("Roguelike Tutorial")
+        .with_font("terminal_10x16.png", 10, 16)
+        .with_tile_dimensions(10, 16)
         .build()?;
     let gs = State {};
     main_loop(context, gs)
